4a50808e7ca0cbd3e520cd5a4a43fb216894f1af
hadal84 <hadal84@proton.me>
2026-06-12 04:07:50 +0300

    remove alfred

--- modules/system/config/alfred/alfred.nix
+++
@@ -1,8 +0,0 @@
-{ config, pkgs, selfPath, ... }:
-
-{
-    home-manager.users.hadal84 = { config, osConfig, ... }: {
-    home.file."Library/Application Support/Alfred/Alfred.alfredpreferences".source =
-      config.lib.file.mkOutOfStoreSymlink "${selfPath}/modules/system/config/alfred/Alfred.alfredpreferences";
-  };
-}
